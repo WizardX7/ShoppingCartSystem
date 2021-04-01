a92a75190932637b661acd5e98871030ff091db6
WizardX7 <60417591+WizardX7@users.noreply.github.com>
2021-04-01 21:46:12 +0530

    Add files via upload

---
+++ eureka-server/src/test/java/com/capgemini/shoppingCartSystem/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.shoppingCartSystem.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product-service/src/test/java/com/capgemini/shoppingCartSystem/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.shoppingCartSystem.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
