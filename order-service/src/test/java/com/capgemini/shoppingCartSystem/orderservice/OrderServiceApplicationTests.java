030d74949d07b6d23573b2388424d5995eca42d5
WizardX7 <60417591+WizardX7@users.noreply.github.com>
2021-04-07 14:32:09 +0530

    Add files via upload

---
+++ order-service/src/test/java/com/capgemini/shoppingCartSystem/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.shoppingCartSystem.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
